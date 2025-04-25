9d348030aecdd1e564b2ef8cb8f4254af612436d
Lucas <lucas.mariano1991@outlook.com>
2025-04-25 15:41:58 -0300

    first commit

---
+++ src/main/java/com/projeto/Sistema_BI/SistemaBiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.Sistema_BI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/Sistema_BI/SistemaBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.Sistema_BI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
